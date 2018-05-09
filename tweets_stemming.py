a1c86fd012df8eea866b086bc9868427f042dbb9
engnaruto <tarekcsed@gmail.com>
2018-05-09 02:53:15 +0200

    Add tweets stemming

---
+++ tweets_stemming.py
@@ -0,0 +1,29 @@
+import json
+
+from py4j.java_gateway import JavaGateway
+
+
+def initialize_stemmer():
+    gateway = JavaGateway()
+    stemmer = gateway.entry_point.getStemmer()
+    return stemmer
+
+
+def run():
+    count = 0
+    with open("data/preprocessed_tweets.json", 'r') as f:
+        with open("data/stemmed_tweets.json", 'w') as w:
+            for line in f:
+                tweet = json.loads(line)
+                text = tweet['text']
+                stemmed_text = stemmer.stem(text)
+                count += 1
+                tweet['text_stemmed'] = stemmed_text
+                json.dump(tweet, w, ensure_ascii=False)
+                w.write('\n')
+                print(count)
+
+
+if __name__ == '__main__':
+    stemmer = initialize_stemmer()
+    run()
