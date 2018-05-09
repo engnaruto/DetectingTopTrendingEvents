05efc5d9fafa37152c7b03ca28e7551c53903aff
engnaruto <tarekcsed@gmail.com>
2018-05-09 03:14:49 +0200

    Add Detecting Top Trending Events

---
+++ DetectingTopTrendingEvents/filter_list.py
@@ -0,0 +1,9 @@
+def build_filter_list():
+    with open("words.txt", "r") as f:
+        words = [line.strip() for line in f]
+        # print(words)
+        return words
+
+
+if __name__ == '__main__':
+    print(build_filter_list())
--- filter_list.py
+++
@@ -1,9 +0,0 @@
-def build_filter_list():
-    with open("words.txt", "r") as f:
-        words = [line.strip() for line in f]
-        # print(words)
-        return words
-
-
-if __name__ == '__main__':
-    print(build_filter_list())
