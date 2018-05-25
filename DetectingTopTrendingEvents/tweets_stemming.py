ba110677dcc93a68846ed74a4b658fce7432bcb4
engnaruto <tarekcsed@gmail.com>
2018-05-25 11:38:44 +0200

    Add stem_event_tweets()

--- DetectingTopTrendingEvents/tweets_stemming.py
+++ DetectingTopTrendingEvents/tweets_stemming.py
@@ -9,7 +9,7 @@ def initialize_stemmer():
     return stemmer
 
 
-def run():
+def stem_tweets():
     count = 0
     with open("data/preprocessed_tweets.json", 'r') as f:
         with open("data/stemmed_tweets.json", 'w') as w:
@@ -24,6 +24,22 @@ def run():
                 print(count)
 
 
+def stem_event_tweets():
+    count = 0
+    with open("data/preprocessed_event_tweets.json", 'r') as f:
+        with open("data/stemmed_event_tweets.json", 'w') as w:
+            for line in f:
+                tweet = json.loads(line)
+                text = tweet['text_preproccesed']
+                stemmed_text = stemmer.stem(text)
+                count += 1
+                tweet['text_stemmed'] = stemmed_text
+                json.dump(tweet, w, ensure_ascii=False)
+                w.write('\n')
+                print(count)
+
+
 if __name__ == '__main__':
     stemmer = initialize_stemmer()
-    run()
+    stem_tweets()
+    stem_event_tweets()
